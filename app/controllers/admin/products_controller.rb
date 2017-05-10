c7e1e333240a1f8e9408df3c0a75afbd57b082ef
greatjing <greatjing@sina.cn>
2017-05-10 23:33:30 +0800

    add admin/product new.html.erb

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,15 @@
 class Admin::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  private
+  def params_product
+
+  end
 end
