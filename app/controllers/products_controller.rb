e2899d521e5f0d216596b86de9a950d4364475b1
郭晶晶 <guojingjing@AT4280.corp.autohome.inc>
2017-05-28 22:18:54 +0800

    add products/edit.html.erb category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,8 +19,6 @@ class ProductsController < ApplicationController
       @category_id = Category.find_by(name: params[:category]).id
       @products = Product.where(category_id: @category_id)
     end
-
-
   end
 
   def show
